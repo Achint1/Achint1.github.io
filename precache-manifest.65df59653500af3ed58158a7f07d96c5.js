d1a2e50474a48645bb291ea260350f9a31c1e0de
achint <Achint1>
2021-09-05 15:42:25 +0530

    FIRST COMMIT

---
+++ precache-manifest.65df59653500af3ed58158a7f07d96c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb02ee61ac03734686002b00cca64776",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "1e234ac2b4f38761de57",
+    "url": "/home/static/css/main.4e794dd0.chunk.css"
+  },
+  {
+    "revision": "d98b825952ae93ba8249",
+    "url": "/home/static/js/2.00aebe82.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.00aebe82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e234ac2b4f38761de57",
+    "url": "/home/static/js/main.cf555397.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "e6574c597d17e9547b1b5a841ede4ed4",
+    "url": "/home/static/media/profile.e6574c59.jpg"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
